3ebba319e758cf31127c860c467c2cb98ba800ba
Michal Štrajt <93714867+mikeshtro@users.noreply.github.com>
2024-07-06 20:31:22 +0200

    Update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { provideContent, withMarkdownRenderer } from '@analogjs/content';
 import { provideFileRouter } from '@analogjs/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { withComponentInputBinding } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideFileRouter(withComponentInputBinding()),
     provideHttpClient(withFetch()),
     provideClientHydration(),
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 /// <reference types="vitest" />
 
-import analog from '@analogjs/platform';
 import { defineConfig } from 'vite';
+import analog from '@analogjs/platform';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  publicDir: 'src/assets',
   build: {
     target: ['es2020'],
   },
@@ -16,7 +15,7 @@ export default defineConfig(({ mode }) => ({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['src/test.ts'],
+    setupFiles: ['src/test-setup.ts'],
     include: ['**/*.spec.ts'],
     reporters: ['default'],
   },
